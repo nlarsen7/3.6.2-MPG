cd84108ba012658c466ae6f4394d9d2396ecee87
nlarsen7 <nlarsen2013@gmail.com>
2020-04-26 01:31:52 +0000

    3.6.2 MPG

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
+//Nicholas Larsen
+//4/25/2020
+//takes your miles and gallons and finds the MPG
 package main
 
 import "fmt"
 
 func divide(miles float64, gallons float64)float64{
   return miles/gallons
+  //your MPG
 }
 func main() {
   miles:=768.0
   gallons:=16.0
+  //sets values for each type
   fmt.Println(divide(miles,gallons),"is your miles to the gallon")
+  //prints out the MPG
 }
